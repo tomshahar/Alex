dc639790111fec66cc4aac8d3d0d47d61f59ea97
Tom Shahar <tomshah@nus11261-12-tomshah.nueva.local>
2019-04-05 09:50:22 -0700

    first push

---
+++ js/common.js
@@ -0,0 +1,5 @@
+function menuToggle() {
+  document.getElementsByClassName("menu-button-bar-container")[0].classList.toggle("menu-button-bar-container--opened");
+  document.getElementsByClassName("fs-menu")[0].classList.toggle("fs-menu--opened");
+
+}
